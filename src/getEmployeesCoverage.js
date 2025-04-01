6dabaa463b8f525d2f74594ba25af9cc413ac58e
p4n1k0 <gabre.tarick@gmail.com>
2025-03-31 21:37:07 -0300

    refatorando

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,28 +2,23 @@
 
 const { employees, species } = require('../data/zoo_data');
 
+// Caso o objeto de opçoes tiver a propriedade name, ela servira tanto para o nome da pessoa quanto para o segundo nome. E caso objeto de opçoes tiver a propriedade id, retornará somente a pessoa correspondente ao id
 function getEmployee(person) {
-  return employees.find((employee) => ( // Caso o objeto de opçoes tiver a propriedade name, ela servira tanto para o nome da pessoa quanto para o segundo nome. E caso objeto de opçoes tiver a propriedade id, retornará somente a pessoa correspondente ao id
-    person.name === employee.firstName
-    || person.name === employee.lastName
-    || person.id === employee.id
-  ));
+  return employees.find((employee) => (person.name === employee.firstName || person.name === employee.lastName || person.id === employee.id));
 }
 
+// Retorna espécies que funcionário é responsável
 function getSpecies(employee) {
-  return employee.responsibleFor.map((id) => ( // Retorna espécies que funcionário é responsável
-    species.find((specie) => specie.id === id)
-  ));
+  return employee.responsibleFor.map((id) => (species.find((specie) => specie.id === id)));
 }
 
 function foundEmployees() {
   return employees.map((employee) => { // Retorna lista de cobertura de todos os funcionários
-    const speci = getSpecies(employee); // Usa função de achar espécies para retornar dentro no objetao
     return {
       id: employee.id,
       fullName: `${employee.firstName} ${employee.lastName}`,
-      species: speci.map((specie) => specie.name),
-      locations: speci.map((specie) => specie.location),
+      species: getSpecies(employee).map((specie) => specie.name),
+      locations: getSpecies(employee).map((specie) => specie.location),
     };
   });
 }
@@ -31,15 +26,12 @@ function foundEmployees() {
 function getEmployeesCoverage(person) {
   if (!person) return foundEmployees(); // Caso nao receba parametros, irá retornar lista de cobertura de todas as pessoas funcionárias
   const employee = getEmployee(person);
-  if (!employee) { // Caso nao haja nenhuma pessoa com o nome ou id especificados, será lançado uma mensagem de erro
-    throw new Error('Informações inválidas');
-  }
-  const speci = getSpecies(employee);
+  if (!employee) throw new Error('Informações inválidas');  
   return { // Retorna a cobertura de pessoas funcionárias com suas devidas propriedades
     id: employee.id,
     fullName: `${employee.firstName} ${employee.lastName}`,
-    species: speci.map((specie) => specie.name),
-    locations: speci.map((specie) => specie.location),
+    species: getSpecies(employee).map((specie) => specie.name),
+    locations: getSpecies(employee).map((specie) => specie.location),
   };
 }
 
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,16 +1,8 @@
 const { employees, species } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) { // Cria variável para armazenar primeiro animal que é gerenciado pelo funcionário do id passado como parâmetro
-  const employeeAnimal = employees.find((employee) => (
-    employee.id === id
-  )).responsibleFor[0];
-  const firstOldest = species.find((specie) => ( // Cria variável para atribuir um array com o nome, sexo e idade do animal mais velho da espécie
-    specie.id === employeeAnimal
-  )).residents.sort((a, b) => b.age - a.age).map((animal) => (
-    [animal.name, animal.sex, animal.age]
-  ))[0];
-
-  return firstOldest;
+function getOldestFromFirstSpecies(id) {
+  return species.find((specie) => (specie.id === employees.find((employee) => (employee.id === id)).responsibleFor[0]
+  )).residents.sort((a, b) => b.age - a.age).map((animal) => ([animal.name, animal.sex, animal.age]))[0];
 }
 
 module.exports = getOldestFromFirstSpecies;
